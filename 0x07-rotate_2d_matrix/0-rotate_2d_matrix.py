92214d6c08dce47ff40b148945c261a077d34c27
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-04 22:03:04 +0100

    Search money

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains a function that rotates an n x n
+matrices without using extra space.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotates matrix clockwisely by 90 degree."""
+    n = len(matrix)
+
+    # Transpose each:
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[j][i]
+
+    # Reverse each row
+    for row in matrix:
+        row.reverse()
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,2 @@
+## Description
+Implement an in-place algorithm to rotate an n x n 2D matrix by 90 degrees clockwise.
