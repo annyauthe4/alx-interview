13ec9f57dfb4dc02f4d6327834f2757b46301268
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-06 02:33:12 +0100

    Correct index

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,7 +12,7 @@ def rotate_2d_matrix(matrix):
     # Transpose each:
     for i in range(n):
         for j in range(i + 1, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[j][i]
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     # Reverse each row
     for row in matrix:
